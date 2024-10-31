cbebddfb6c15a25b7bc13aea40547e0e731967d5
yumumakaron <matchirolll6802@gmail.com>
2024-10-31 13:07:30 +0900

    update index and application

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,16 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// トップに移動する固定ボタン
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('fixed_button2top');
+    if (button) {
+      button.addEventListener('click', function() {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      });
+    }
+  });
