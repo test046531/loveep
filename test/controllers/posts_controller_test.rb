a284b2f2ee5d18c15ec2312bc6de8fa30ad0b10a
tde <tde4342@gmail.com>
2024-11-04 02:07:49 +0900

    Add /posts/new page

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,7 +2,12 @@ require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get posts_index_url
+    get posts_path
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_post_path
     assert_response :success
   end
 end
