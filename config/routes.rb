2064bf45c364fb0a962687909a45b9566dd3f4da
tde <tde4342@gmail.com>
2024-12-07 04:06:54 +0900

    Add seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
+  get 'posts', to: 'home#posts'
   # root "posts#index"
-  resources :posts, only: [:index, :new, :create]
+  resources :users do
+    resources :posts
+  end
   get 'question', to: 'pages#question'
   get 'mypage', to: 'users#mypage'
   
