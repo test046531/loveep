f446e37b0bde38ff185e968fceb639ddae05caee
tde <tde4342@gmail.com>
2025-02-07 00:11:44 +0900

    Fix Session Errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_03_161001) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_06_150244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_03_161001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.text "image_name"
+    t.string "image_name"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_03_161001) do
     t.string "status_message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
   add_foreign_key "posts", "users"
