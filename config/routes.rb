b5e6d3f9b2c46dc636c11862fbbefa2502be6821
tde <tde4342@gmail.com>
2025-03-07 00:32:52 +0900

    Create users-controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root "home#index"
   get 'posts', to: 'home#posts'
   # root "posts#index"
-  resources :users do
+  resources :users, only: [:create] do
     resources :posts
   end
   get 'question', to: 'pages#question'
   get 'mypage', to: 'users#mypage'
+  get 'signup', to: 'users#new'
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
