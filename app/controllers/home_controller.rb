a835f7d4e955bf42921849bc8c9482a04f8ddcec
yumumakaron <matchirolll6802@gmail.com>
2025-03-27 19:19:51 +0900

    投稿詳細ページの更新

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
     def index
-        @posts = Post.limit(20) # 取得投稿数を20に制限
+        @posts = Post.order(created_at: :desc).limit(20) # 新しい順に20件取得
     end
 
     def posts
